a837f8abcea197deafe587277345b5f602a231ea
RaviCarrillo <ravicarrillo@gmail.com>
2024-02-20 20:15:12 -0300

    create petshop

---
+++ src/paginas/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const Home = () => {
+    return (
+        <main>
+            <div>
+                <h2>Pet Notícias</h2>
+            </div>
+            <section>
+                <article>
+                    <h2>Banho no cão</h2>
+                    <p>Banhar regularmente os cães traz uma série de benefícios para a saúde e bem-estar deles. Além de manter a pelagem limpa e com aspecto saudável, os banhos ajudam a remover sujeira, resíduos e odores indesejados. Também contribuem para a prevenção de problemas de pele, como coceiras e infecções. Além disso, o banho proporciona um momento de cuidado e carinho entre o tutor e o animal, fortalecendo o vínculo afetivo. É importante utilizar produtos adequados para cães e seguir as recomendações do veterinário para garantir a segurança e o conforto durante o banho. </p>
+                </article>
+            </section>
+        </main>
+    )
+
+}
+
+export default Home;
