5a37ad01923a8d9617621d68b0a010013b147534
RaviCarrillo <ravicarrillo@gmail.com>
2024-02-21 20:38:33 -0300

    creating routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import React from 'react';
 import './App.css';
 import Home from './paginas/home';
 import Sobre from './paginas/sobre';
+import NotFound from './paginas/notfound';
 import './assets/css/base.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 function App() {
+  return (
+   
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/sobre" component={Sobre} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router> 
+
+  );
+} 
+
+
+
+/* function App() {
+  const Router = () => {
+    const location = window.location.pathname;
+    if(location === '/sobre'){
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  }
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+} */
+
+
+/* function App() {
   return (
     <div className="App">
       <div>
@@ -14,6 +49,6 @@ function App() {
       </div>
     </div>
   );
-}
+} */
 
 export default App;
---
+++ src/paginas/notfound.jsx
@@ -0,0 +1,19 @@
+import React from "react"; 
+
+const NotFound = () => {
+    return (
+        <main>
+            <div>
+                <h2>404 - Página não encontrada</h2>
+            </div>
+            <section>
+                <article>
+                    <h2>Ops! Parece que você se perdeu...</h2>
+                    <p>A página que você está procurando não existe ou foi removida. Por favor, verifique o endereço digitado ou retorne à página inicial.</p>
+                </article>
+            </section>
+        </main>
+    )
+}
+
+export default NotFound;
